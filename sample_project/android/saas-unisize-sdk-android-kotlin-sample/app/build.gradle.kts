05615479b30274b9c8fb2d3f89f1db48b32a05f6
Makip <h.ashihara@makip.co.jp>
2025-04-16 18:22:32 +0900

    Update Sample Projects

--- sample_project/android/saas-unisize-sdk-android-kotlin-sample/app/build.gradle.kts
+++ sample_project/android/saas-unisize-sdk-android-kotlin-sample/app/build.gradle.kts
@@ -47,7 +47,7 @@ dependencies {
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
 
-    implementation("jp.co.makip:unisize-sdk:1.5.5")
+    implementation("jp.co.makip:unisize-sdk:2.0")
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
